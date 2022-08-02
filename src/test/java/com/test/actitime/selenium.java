0cbad08484622d0803a84789bfd42882bad59bd9
sahil <sahil@DESKTOP-116BRHL.mshome.net>
2022-08-02 22:11:36 +0530

    another test class pushed

---
+++ src/test/java/com/test/actitime/selenium.java
@@ -0,0 +1,13 @@
+package com.test.actitime;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class selenium 
+{
+	@Test
+	public void selen() {
+		
+		Reporter.log("selenium last classes",true);
+	}
+}
